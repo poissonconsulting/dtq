65a1c88a705cd6f18c0029d5d04640a29434d57f
Joe Thorley <joe@poissonconsulting.ca>
2018-11-14 17:15:21 -0800

    fix remotes

---
+++ R/drop.R
@@ -0,0 +1,22 @@
+
+dtq_add_drop <- function(x,  dtt = "DateTime", colname = "Discharge",
+                         start = x[[dtt]][1], duration = 1L,
+                         target = 0, rate_down = Inf, rate_up = rate_down,
+                         units = dtt_units(x[[dtt]])) {
+  check_string(colname)
+  check_dtq(x, dtt = dtt, colname = colname, 
+            complete = TRUE, sorted = TRUE, unique = TRUE, units = units)
+  if(!nrow(x)) return(x)
+  check_dtt(start, length = TRUE, unique = TRUE, sorted = TRUE, 
+            tz = dtt_tz(x[[dtt]]))
+  if(!all(start %in% x[[dtt]])) 
+    err("start must be in ", dtt, " column")
+  
+  length <- c(1L, 1L, length(start))
+  check_vector(target, c(0, chk_max_dbl()), length = length)
+  check_vector(rate_down, c(0, chk_max_dbl()), length = length)
+  check_vector(rate_up, c(chk_tiny_dbl(), chk_max_dbl()), length = length)
+  
+
+  
+}
