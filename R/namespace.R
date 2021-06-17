55c590feb415aa377eb40b273e6d73a1850b1b27
Joe Thorley <joe@poissonconsulting.ca>
2021-06-17 16:54:06 -0700

    added chk

--- R/namespace.R
+++ R/namespace.R
@@ -1,2 +1,5 @@
-#' @import chk dttr dts
+#' @import dttr dts
+#' @importFrom chk chk_flag
+#' @importFrom checkr check_data check_string chk_deparse check_vector check_pos_dbl chk_max_dbl check_count check_noneg_dbl check_pos_int check_unique chk_tiny_dbl
+#' @importFrom err err wrn
 NULL
