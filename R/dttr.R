20153674728181ab34b3b948cc299988c26b1adb
Joe Thorley <joe@poissonconsulting.ca>
2021-06-21 13:49:45 -0700

    simplify dtt_disaggregate

--- R/dttr.R
+++ R/dttr.R
@@ -1,29 +1,4 @@
-# added as hack for tests
-
 dtt_disaggregate <- function(x, ...) {
-  UseMethod("dtt_disaggregate")
-}
-
-dtt_disaggregate.Date <- function(x, ...) {
-  units <- dtt_units(x)
-  if(identical(units, "days"))
-    err("Date vectors cannot be disaggregated below 'days'")
-  end <- x[length(x)]
-  
-  if(identical(units, "months")) {
-    end <- dtt_add_months(end)
-    end <- dtt_subtract_days(end)
-  } else {
-    end <- dtt_add_years(end)
-    end <- dtt_subtract_months(end)
-  }
-  x <- c(x, end)
-  x <- dtt_floor(x)
-  x <- dtt_complete(x)
-  x
-}
-
-dtt_disaggregate.POSIXct <- function(x, ...) {
   units <- dtt_units(x)
   if(identical(units, "seconds"))
     err("POSIXct vectors cannot be disaggregated below 'seconds'")
